44f2d0e5d2289e808ecd6e2947a6f4e37b237068
Dhruv P <169485603+dhruvp987@users.noreply.github.com>
2025-04-07 00:07:02 -0400

    Stylized name and title in index

--- src/NameTitle.tsx
+++ src/NameTitle.tsx
@@ -1,8 +1,10 @@
+import './NameTitle.css';
+
 function NameTitle() {
   return (
     <div className="name-title-div">
-      <h1 className="name-title">Dhruv Patel,</h1>
-      <h1 className="name-title">Connoisseur of Everything</h1>
+      <h1 className="name-title-name">Dhruv Patel,</h1>
+      <h1 className="name-title-title">Connoisseur of Everything</h1>
     </div>
   );
 }
