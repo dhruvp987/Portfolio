06be8762f89bf81e8021f29002e19d43c4c5571b
Dhruv P <169485603+dhruvp987@users.noreply.github.com>
2025-04-06 23:03:42 -0400

    Increased wind length in index
    The faster wind's length was increased slightly, while the slower
    wind's length was increased significantly to make it look heavier.

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Wind startY={100} width={75} height={5} aniDur={35} />
-    <Wind startY={110} width={75} height={5} aniDur={45} />
+    <Wind startY={100} width={85} height={5} aniDur={35} />
+    <Wind startY={110} width={125} height={5} aniDur={45} />
     <NameTitle />
     <MainMenu />
     <RandomPokeCard />
