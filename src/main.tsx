12b299c7b9c1a9f349bb24339573e1f7e67f710a
Dhruv P <169485603+dhruvp987@users.noreply.github.com>
2025-04-06 23:13:04 -0400

    Increased distance between winds in index

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Wind startY={100} width={85} height={5} aniDur={35} />
-    <Wind startY={110} width={125} height={5} aniDur={45} />
+    <Wind startY={120} width={125} height={5} aniDur={45} />
     <NameTitle />
     <MainMenu />
     <RandomPokeCard />
