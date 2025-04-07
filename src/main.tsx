3e4b4bf287e6ab8ad5593210a7c83e700092edf0
Dhruv P <169485603+dhruvp987@users.noreply.github.com>
2025-04-07 14:05:56 -0400

    Added third wind and made them more visible

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,9 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Wind startY={100} width={85} height={5} aniDur={35} />
-    <Wind startY={120} width={125} height={5} aniDur={45} />
+    <Wind startY={100} width={85} height={10} aniDur={10} />
+    <Wind startY={120} width={125} height={10} aniDur={20} />
+    <Wind startY={130} width={105} height={10} aniDur={15} />
     <NameTitle />
     <MainMenu />
     <RandomPokeCard />
