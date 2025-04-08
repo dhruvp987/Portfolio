6e53404f4a5d5af1a584a6aef16fd336f29c7e22
Dhruv P <169485603+dhruvp987@users.noreply.github.com>
2025-04-08 13:01:13 -0400

    Added LinkedIn and GitHub links to main menu

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -5,6 +5,8 @@ function MainMenu() {
     <div className="main-menu-div">
       <ul className="main-menu-ul">
         <li><a href="/projects">Projects</a></li>
+	<li><a href="https://linkedin.com/in/dhruv-patel-327042293">LinkedIn</a></li>
+	<li><a href="https://github.com/dhruvp987">GitHub</a></li>
       </ul>
     </div>
   );
