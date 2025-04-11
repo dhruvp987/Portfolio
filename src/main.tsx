91473a84f68d2b1866269b4a87e88bac7459e98a
Dhruv P <169485603+dhruvp987@users.noreply.github.com>
2025-04-10 22:30:43 -0400

    Specified basename in React Router
    This simplifies the basename handling, as the rest of the source
    does not need to be aware of it.

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import './main.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
       <Routes>
-        <Route path="/Portfolio" element={<Index />} />
-	<Route path="/Portfolio/Projects" element={<Projects />} />
+        <Route path="/" element={<Index />} />
+	<Route path="/Projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
