5e775f7d5ceb59ccd3a4d8cd349fa250b1a94bcc
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-08-20 16:19:58 -0400

    Format code with Prettier

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -4,27 +4,53 @@
 	let quoteVisible = $state(true);
 	let mainContentVisible = $state(false);
 
-	setTimeout(() => { quoteVisible = false; }, 4000);
-	setTimeout(() => { mainContentVisible = true; }, 4500);
+	setTimeout(() => {
+		quoteVisible = false;
+	}, 4000);
+	setTimeout(() => {
+		mainContentVisible = true;
+	}, 4500);
 </script>
 
-<div class="bg-[url('/ambition.png')] w-screen h-screen absolute -z-1 bg-no-repeat bg-bottom bg-cover brightness-35"></div>
+<div
+	class="absolute -z-1 h-screen w-screen bg-[url('/ambition.png')] bg-cover bg-bottom bg-no-repeat brightness-35"
+></div>
 
 {#if quoteVisible}
-	<div class="w-screen h-screen p-5 flex flex-col items-center justify-center" transition:fade>
-		<h1 class="block instrument-serif-regular text-white text-7xl">The only way to do great work is to love what you do.</h1>
-		<h1 class="block instrument-serif-regular text-white text-7xl">~ Steve Jobs</h1>
+	<div class="flex h-screen w-screen flex-col items-center justify-center p-5" transition:fade>
+		<h1 class="instrument-serif-regular block text-7xl text-white">
+			The only way to do great work is to love what you do.
+		</h1>
+		<h1 class="instrument-serif-regular block text-7xl text-white">~ Steve Jobs</h1>
 	</div>
 {/if}
 
 {#if mainContentVisible}
-	<div class="w-screen h-screen flex flex-col items-center justify-center" transition:fade>
-		<h1 class="instrument-serif-regular text-white text-7xl p-11 grow">Dhruv Patel</h1>
-		<h2 class="instrument-serif-regular text-white text-5xl p-6 flex-none">Building the Fun Stuff for Everyone 🚀</h2>
-		<div class="pb-11 flex-none">
-			<a href="/projects" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1 transition duration-200 ease-in-out hover:bg-cyan-100">Projects</a>
-			<a href="https://github.com/dhruvp987" target="_blank" rel="noopener noreferrer" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1 transition duration-200 ease-in-out hover:bg-cyan-100">GitHub</a>
-			<a href="https://linkedin.com/in/dhruvpatel789" target="_blank" rel="noopener noreferrer" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1 transition duration-200 ease-in-out hover:bg-cyan-100">LinkedIn</a>
+	<div class="flex h-screen w-screen flex-col items-center justify-center" transition:fade>
+		<h1 class="instrument-serif-regular grow p-11 text-7xl text-white">Dhruv Patel</h1>
+		<h2 class="instrument-serif-regular flex-none p-6 text-5xl text-white">
+			Building the Fun Stuff for Everyone 🚀
+		</h2>
+		<div class="flex-none pb-11">
+			<a
+				href="/projects"
+				class="space-mono-regular mx-1 inline-block rounded-3xl bg-white px-4 py-2 text-xl text-black transition duration-200 ease-in-out hover:bg-cyan-100"
+				>Projects</a
+			>
+			<a
+				href="https://github.com/dhruvp987"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="space-mono-regular mx-1 inline-block rounded-3xl bg-white px-4 py-2 text-xl text-black transition duration-200 ease-in-out hover:bg-cyan-100"
+				>GitHub</a
+			>
+			<a
+				href="https://linkedin.com/in/dhruvpatel789"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="space-mono-regular mx-1 inline-block rounded-3xl bg-white px-4 py-2 text-xl text-black transition duration-200 ease-in-out hover:bg-cyan-100"
+				>LinkedIn</a
+			>
 		</div>
 	</div>
 {/if}
