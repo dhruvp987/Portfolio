9fde460b91133f3b1e8fbff22cfe6b701d9cc6b2
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-08-20 01:20:54 -0400

    Add button hover transition

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -3,8 +3,8 @@
 	<h1 class="instrument-serif-regular text-white text-7xl p-10 grow">Dhruv Patel</h1>
 	<h2 class="instrument-serif-regular text-white text-5xl p-5 flex-none">Building the Fun Stuff for Everyone</h2>
 	<div class="pb-10 flex-none">
-		<a href="/projects" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1">Projects</a>
-		<a href="https://github.com/dhruvp987" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1">GitHub</a>
-		<a href="https://linkedin.com/in/dhruvpatel789" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1">LinkedIn</a>
+		<a href="/projects" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1 transition duration-200 ease-in-out hover:bg-cyan-100">Projects</a>
+		<a href="https://github.com/dhruvp987" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1 transition duration-200 ease-in-out hover:bg-cyan-100">GitHub</a>
+		<a href="https://linkedin.com/in/dhruvpatel789" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1 transition duration-200 ease-in-out hover:bg-cyan-100">LinkedIn</a>
 	</div>
 </div>
