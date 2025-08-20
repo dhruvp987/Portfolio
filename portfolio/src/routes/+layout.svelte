01c49eceb7139325ad6dd23867ee2e260921b336
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-08-20 01:11:37 -0400

    Create prototype look

--- portfolio/src/routes/+layout.svelte
+++ portfolio/src/routes/+layout.svelte
@@ -7,6 +7,9 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Instrument+Serif&family=Space+Mono&display=swap" rel="stylesheet" />
 </svelte:head>
 
 {@render children?.()}
--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg-[url('/ambition.png')] w-screen h-screen absolute -z-1 bg-no-repeat bg-bottom bg-cover brightness-50"></div>
+<div class="w-screen h-screen flex flex-col items-center justify-items-center">
+	<h1 class="instrument-serif-regular text-white text-7xl p-10 grow">Dhruv Patel</h1>
+	<h2 class="instrument-serif-regular text-white text-5xl p-5 flex-none">Building the Fun Stuff for Everyone</h2>
+	<div class="pb-10 flex-none">
+		<a href="/projects" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1">Projects</a>
+		<a href="https://github.com/dhruvp987" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1">GitHub</a>
+		<a href="https://linkedin.com/in/dhruvpatel789" class="inline-block space-mono-regular bg-white text-black text-xl px-4 py-2 rounded-3xl mx-1">LinkedIn</a>
+	</div>
+</div>
